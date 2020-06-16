a58c5b8b04d234b0fed263371c98701d0571cf84
Matthew Mattox <matthew.mattox@rancher.com>
2020-06-15 22:59:27 -0500

    Updating overlay test

--- agent/check-overlay.sh
+++ agent/check-overlay.sh
@@ -35,5 +35,12 @@ fi
 
 for ip in $(kubectl describe endpoints suport-agent --namespace=cattle-system | grep ' Addresses:' | awk '{print $2}' | sed 's/,/\n/g')
 do
-  echo "Checking $ip"
+  Output=`timeout "$Timeout" ping -c 1 "$ip"`
+  Result=$?
+  if [ $Result -eq 0 ]
+  then
+    echo "$ip is pingable"
+  else
+    echo "$ip is not pingable"
+  fi
 done
