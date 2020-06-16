f5ef2b93a4525f0ee56280bd7e827b097dc8ec4d
Matthew Mattox <matthew.mattox@rancher.com>
2020-06-15 21:07:18 -0500

    mmattox

---
+++ agent/check-overlay.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 options
+OPTIONS:
+   -h      Show this message
+   -T      DNS Timeout in seconds Default: 10s
+EOF
+}
+
+VERBOSE=
+while getopts .ht:T:v. OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         T)
+             Timeout=$OPTARG
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+if [[ -z $Timeout ]]
+then
+  Timeout=10s
+fi
+
+for ip in $(kubectl describe endpoints suport-agent --namespace=cattle-system | grep ' Addresses:' | awk '{print $2}' | sed 's/,/\n/g')
+do
+  echo "Checking $ip"
+done
