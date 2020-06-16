88d55615d134dc3d4d8a6d18e759674f16371e9b
Matthew Mattox <matthew.mattox@rancher.com>
2020-06-16 00:59:58 -0500

    Updating manager

--- build.sh
+++ build.sh
@@ -8,5 +8,12 @@ docker push cube8021/rancher-support-tools-agent:"$BUILD_NUMBER"
 cat ./deployment.yml | sed "s/BUILD_NUMBER/$BUILD_NUMBER/g" | kubectl apply -f -
 cd ../
 
+echo "Docker build and deploy for manager...."
+cd ./agent
+docker build -t cube8021/rancher-support-tools-manager:"$BUILD_NUMBER" .
+docker push cube8021/rancher-support-tools-manager:"$BUILD_NUMBER"
+cat ./deployment.yml | sed "s/BUILD_NUMBER/$BUILD_NUMBER/g" | kubectl apply -f -
+cd ../
+
 echo "Cleaning up workspace..."
 git clean -f
